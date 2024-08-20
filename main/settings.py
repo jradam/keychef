89c0db443d8274a9129519e5d88e57a90e3f3e6f
jradam <15231996+jradam@users.noreply.github.com>
2024-08-20 14:32:18 -0400

    added user settings

--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,30 @@
-from winput import VK_OEM_1, VK_F13
+from main.helpers import get_keycode
 
-activate_key = VK_OEM_1
-on_activate = VK_F13
+# User settings
+activate_key = get_keycode("semicolon")
+on_activate = get_keycode("F13")
+replace_key = "c"
+
+# Binds that are always active
+permanent_binds = [
+    {"key": "capslock", "output": "escape"},
+]
+
+# Binds for when the keychef layer is active
+layer_binds = [
+    {"key": "m", "output": "dollar"},
+    {"key": "f", "output": "brace_open"},
+    {"key": "j", "output": "brace_close"},
+    {"key": "d", "output": "parenthesis_open"},
+    {"key": "k", "output": "parenthesis_close"},
+    {"key": "s", "output": "bracket_open"},
+    {"key": "l", "output": "bracket_close"},
+    {"key": "g", "output": "minus"},
+    {"key": "u", "output": "underscore"},
+    {"key": "a", "output": "ampersand"},
+    {"key": "e", "output": "plus"},
+    {"key": "n", "output": "return"},
+    {"key": "space", "output": "backspace"},
+    {"key": "x", "output": "delete"},
+    {"key": "capslock", "output": "capslock"},
+]
