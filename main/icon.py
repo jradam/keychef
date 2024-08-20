995b127130df2a447c898889f9b5e2de3219de29
jradam <15231996+jradam@users.noreply.github.com>
2024-08-20 11:53:16 -0400

    switched icon to use lambda functions

--- main/icon.py
+++ main/icon.py
@@ -4,16 +4,12 @@ import pystray
 
 
 def setup_icon(state) -> pystray.Icon:
-    def click(icon, item):
-        if str(item) == "Exit":
-            icon.stop()
-            state.exit()
-
     icon = pystray.Icon(
         "Keychef",
         PIL.Image.open(absolute_path("icon.png")),
         menu=pystray.Menu(
-            pystray.MenuItem("Settings", click), pystray.MenuItem("Exit", click)
+            pystray.MenuItem("Settings", lambda: print("Settings clicked")),
+            pystray.MenuItem("Exit", lambda: (icon.stop(), state.exit())),
         ),
     )
 
