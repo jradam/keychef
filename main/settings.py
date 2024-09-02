f44474fedf337ed721b30ab0c7de8409abe6f141
jradam <15231996+jradam@users.noreply.github.com>
2024-09-02 09:48:31 -0400

    sorted setting keys, added blanks

--- main/settings.py
+++ main/settings.py
@@ -13,23 +13,31 @@ permanent_binds: BindList = [
 
 # Binds for when the KeyChef layer is active
 layer_binds: BindList = [
-    {"key": "i", "output": "minus"},
     {"key": "a", "output": "ampersand"},
-    {"key": "d", "output": "parenthesis_close"},
-    {"key": "capslock", "output": "capslock"},
     {"key": "b", "output": "bracket_close"},
-    {"key": "s", "output": "parenthesis_open"},
+    # {"key": "c", "output": ""}, # In use above
+    {"key": "d", "output": "parenthesis_close"},
     {"key": "e", "output": "equals"},
     {"key": "f", "output": "brace_open"},
     {"key": "g", "output": "brace_close"},
     {"key": "h", "output": "left"},
+    {"key": "i", "output": "minus"},
     {"key": "j", "output": "down"},
     {"key": "k", "output": "up"},
     {"key": "l", "output": "right"},
     {"key": "m", "output": "dollar"},
     {"key": "n", "output": "return"},
-    {"key": "v", "output": "bracket_open"},
-    {"key": "space", "output": "backspace"},
+    {"key": "o", "output": ""},
+    {"key": "p", "output": ""},
+    # {"key": "q", "output": ""}, # In use above
+    {"key": "r", "output": ""},
+    {"key": "s", "output": "parenthesis_open"},
+    {"key": "t", "output": ""},
     {"key": "u", "output": "underscore"},
+    {"key": "v", "output": "bracket_open"},
+    {"key": "w", "output": "capslock"},
     {"key": "x", "output": "delete"},
+    {"key": "y", "output": ""},
+    {"key": "z", "output": ""},
+    {"key": "space", "output": ""},
 ]
